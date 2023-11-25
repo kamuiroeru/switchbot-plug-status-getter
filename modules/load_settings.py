d981eec9d956a8b94c5c5e5d7fd34002bb713724
kamuiroeru <19339328+kamuiroeru@users.noreply.github.com>
2023-11-25 21:57:42 +0900

    [update] Plug のデバイスIDも設定ファイルで読み込むように変更

--- modules/load_settings.py
+++ modules/load_settings.py
@@ -6,15 +6,21 @@ from pathlib import Path
 class Setting:
     api_token: str
     api_secret: str
+    plug_device_ids: list[str]
 
 
-def load_ini(filepath: str) -> dict[str, str]:
+def parse_to_list_str(raw: str) -> list[str]:
+    return list(map(lambda elem: elem.strip(), raw.split(',')))
+
+
+def load_ini(filepath: str) -> dict:
     config = configparser.ConfigParser()
     config.read(filepath)
     default = config['DEFAULT']
     return {
         'api_token': default['API_TOKEN'],
         'api_secret': default['API_SECRET'],
+        'plug_device_ids': parse_to_list_str(default['PLUG_DEVICE_IDS'])
     }
 
 
--- tests/test_load_settings.py
+++ tests/test_load_settings.py
@@ -7,7 +7,8 @@ class TestLoadSettings(unittest.TestCase):
     def test_load_ini(self):
         expected = {
             'api_token': '[GET_TOKEN_FROM_SWITCHBOT_APP]',
-            'api_secret': '[GET_CLIENT_SECRET_FROM_SWITCHBOT_APP]'
+            'api_secret': '[GET_CLIENT_SECRET_FROM_SWITCHBOT_APP]',
+            'plug_device_ids': ['DEVICE_ID1', 'DEVICE_ID2', '...'],
         }
         p = Path(__file__).parent.parent / 'settings.ini.template'
         actual = load_ini(p.as_posix())
